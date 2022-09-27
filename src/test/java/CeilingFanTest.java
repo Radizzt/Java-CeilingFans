5b5e68d569db59c83e66b3aaad59a09de8e5ed54
Richard Huynh <Richardthienhuynh@gmail.com>
2022-09-27 11:26:45 -0400

    Added a few more unit test cases.

--- src/test/java/CeilingFanTest.java
+++ src/test/java/CeilingFanTest.java
@@ -9,12 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CeilingFanTest {
     @ParameterizedTest
     @ValueSource(ints = {0,1,2})
-    @DisplayName("Test Speed Cord")
+    @DisplayName("Test Fan Speed New Speed After Pulled Method")
     public void testSpeedCord(int speed){
         CeilingFan ceilingFan = new CeilingFan();
         ceilingFan.setSpeed(speed);
         ceilingFan.pullSpeedCord();
-        assertEquals(speed == 3 ? 0 : speed + 1, ceilingFan.getSpeed());
+        assertEquals(speed + 1, ceilingFan.getSpeed());
     }
     @Test
     @DisplayName("Test Speed Reset Back to Zero")
@@ -24,6 +24,31 @@ public class CeilingFanTest {
         ceilingFan.pullSpeedCord();
         assertEquals(0, ceilingFan.getSpeed());
     }
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3,4,5})
+    @DisplayName("Test Speed Cord Multiple Time, Make sure Direction Never Change (Forward)")
+    public void testMultiSpeedCordPullAndDirection(int speed){
+        CeilingFan ceilingFan = new CeilingFan();
+        for (int i = 0; i < speed; i++) {
+            ceilingFan.pullSpeedCord();
+        }
+        assertEquals(speed > 3 ? speed % 4 : speed, ceilingFan.getSpeed());
+        assertFalse(ceilingFan.isReverse());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3,4,5})
+    @DisplayName("Test Speed Cord Multiple Time, Make sure Direction Never Change (Reverse)")
+    public void testMultiSpeedCordPullAndDirectionReverse(int speed){
+        CeilingFan ceilingFan = new CeilingFan();
+        ceilingFan.pullReverseCord();
+        for (int i = 0; i < speed; i++) {
+            ceilingFan.pullSpeedCord();
+        }
+        assertEquals(speed > 3 ? speed % 4 : speed, ceilingFan.getSpeed());
+        assertTrue(ceilingFan.isReverse());
+    }
+
     @Test
     @DisplayName("Test Reverse Cord: False to True")
     public void testReverseCordFalseToTrue(){
